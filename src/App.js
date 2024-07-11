c8a6fe94c5685d4af487374f1243507896476eb1
SangJee Park <sjpark3927@gmail.com>
2024-07-12 03:02:47 +0900

    need to modify

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useEffect, useState } from 'react';
+import ClipLoader from "react-spinners/ClipLoader";
 import './App.css';
 import WeatherBox from './component/WeatherBox';
 import WeatherButton from './component/WeatherButton';
@@ -15,8 +16,11 @@ import WeatherButton from './component/WeatherButton';
 
 function App() {
 
-  const [weather, seaWeather] = useState(null)
+  const [weather, setWeather] = useState(null)
   const cities = ['Paris', 'New York', 'Tokyo', 'Seoul'];
+  const [city, setCity]= useState("");
+  const [loading, setLoading] =useState(false);
+
 
     // 현재 위치 
   const getCurrentLocation =()=>{
@@ -30,24 +34,45 @@ function App() {
 
   const getWeatherByCurrentLocation = async(lat, lon)=>{
     let url = (`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=12f16c18bca3e6647c2b6d2cfd03a0d8&units=metric`) 
-    let response = await fetch(url)
+    setLoading(true)
+    let response = await fetch(url);
     let data = await response.json(); 
-    seaWeather(data)
+    setWeather(data)
+    setLoading(false)
   };
 
 
-  useEffect(()=>{
-    getCurrentLocation()
-  }, []);
+  // 버튼누르면 city이름을 가져와 city에 대한 정보 불러오기
+  const getWeatherByCity = async() =>{
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=12f16c18bca3e6647c2b6d2cfd03a0d8&units=metric`;
+    setLoading(true)
+    let response = await fetch(url);
+    let data = await response.json();
+    setWeather(data);
+    setLoading(false);
+  }
+  
+
+  // useState("") 초기값을 정하지 않았기 때문에 useEffect()를 따로 쓰면 정보 불러오기 X -> if문을 이용하여 오류 해결
+  useEffect(() => {
+    if(city === ""){
+      getCurrentLocation();
+    }else{
+      getWeatherByCity();
+    }
+  }, [city]);
+
 
   return (
     <div>
       <div className='title'>How's the Weather?</div>
-      <div className='container'>
+
+      <div>{loading?<div className='container'><ClipLoader color="#f88c6b" loading={loading} size={150}/></div>:<div className='container'>
         <WeatherBox weather ={weather} />
-        <WeatherButton cities = {cities} />
-      </div>
+        <WeatherButton cities = {cities} getCurrentLocation = {getCurrentLocation} setCity={setCity}/>
+      </div>}
       <div className='image'></div>
+    </div>
     </div>
   );
 }
@@ -55,3 +80,8 @@ function App() {
 export default App;
 
 // 날씨 별로 움짤 변경되면 좋겠당
+
+// 버튼 클릭
+// 해당 버튼의 색깔 유지
+// 함수 필요할 듯
+// 
--- src/component/WeatherButton.js
+++ src/component/WeatherButton.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 
+// app.js에 있는 setCity 함수를 갖고 온다. 여기서 사용하기만 한다.
 
-const WeatherButton = ({cities}) => {
-    console.log("cities", cities);
+const WeatherButton = ({cities, setCity, getCurrentLocation}) => {
   return (
     <div>
-      <Button variant="light">Current Location</Button>
-      
+      <Button variant="light" setCity={null} onClick={()=>{getCurrentLocation()}}>Current Location</Button>
       {cities.map((item)=>(
-      <Button variant='light'>{item}</Button>))}
+      <Button variant="light" 
+      onClick={()=>setCity(item)}> 
+      {item}</Button>))}   
     </div>
   )
 }
-
 
 export default WeatherButton
