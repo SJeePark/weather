ce03a87f0b6b52691c548176ddc64c1f512031b0
SangJee Park <sjpark3927@gmail.com>
2024-07-15 02:50:58 +0900

    change background

--- src/App.js
+++ src/App.js
@@ -15,13 +15,13 @@ import WeatherButton from './component/WeatherButton';
 
 
 function App() {
-
   const [weather, setWeather] = useState(null)
-  const cities = ['Canberra', 'New York', 'Tokyo', 'London'];
+  const cities = ['Ulan Bator','Madrid', 'Tokyo', 'London'];
   const [city, setCity]= useState("");
   const [loading, setLoading] =useState(false);
   const [selectedButton, setSelectedButton] = useState("");
   const [apiError, setAPIError] = useState("");
+  const [icon, setIcon] = useState("")
 
 
     // 현재 위치 
@@ -34,12 +34,14 @@ function App() {
     });
   }
 
+
   const getWeatherByCurrentLocation = async(lat, lon)=>{
-    try{let url = (`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=12f16c18bca3e6647c2b6d2cfd03a0d8&units=metric`) 
+    try{let url = (`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=12f16c18bca3e6647c2b6d2cfd03a0d8&units=metric&lang=kr`) 
     setLoading(true)
     let response = await fetch(url);
     let data = await response.json(); 
     setWeather(data);
+    setIcon(data.weather[0].icon);
     setLoading(false);
     } catch (err) {
       setAPIError(err.message);
@@ -51,11 +53,12 @@ function App() {
   // 버튼누르면 city이름을 가져와 city에 대한 정보 불러오기
   const getWeatherByCity = async() =>{
     try {
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=12f16c18bca3e6647c2b6d2cfd03a0d8&units=metric&lang`;
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=12f16c18bca3e6647c2b6d2cfd03a0d8&units=metric&lang=kr`;
     setLoading(true)
     let response = await fetch(url);
     let data = await response.json();
     setWeather(data);
+    setIcon(data.weather[0].icon);
     setLoading(false);
     } catch (err) {
       console.log(err);
@@ -79,22 +82,29 @@ function App() {
   const handleButtonClick = (cityName) => {
     setCity(cityName);
     setSelectedButton(cityName);
-  };
+  };  
 
+  // 날씨 별 배경 변환
+  const changeBackground=()=>{
+    <body url={''}></body>
+  }
 
   return (
     <div>
+      <body className={`body${icon}`}>
       <div className='title'>How's the Weather?</div>
       <div className='container'>
         {loading && <ClipLoader color="violet" loading={loading} size={150} />}
         {apiError && <div className="error-message">{apiError}</div>}
         {!loading && !apiError && (
           <>
-            <WeatherBox weather={weather} />
+            <WeatherBox weather={weather} icon={icon} />
             <WeatherButton cities={cities} getCurrentLocation={getCurrentLocation} setCity={handleButtonClick} selectedButton={selectedButton} />
           </>
+          
         )}
       </div>
+      </body>
     </div>
   );
 }
--- src/component/WeatherBox.js
+++ src/component/WeatherBox.js
@@ -1,34 +1,41 @@
 import React from 'react';
+import './WeatherBox.css';
 
-const WeatherBox = ({weather}) => {
+
+const WeatherBox = ({ weather, icon }) => {
   console.log('weather?', weather);
   return (
-    <div>
     <div className='weather-box'>
-      <div style={{fontWeight: 1000, fontSize:'30px', marginBottom:'15px', justifyContent:'center', display:'flex'}}>{weather?.sys.country} {weather?.name}</div>
-      <h2 className='temp'>{Math.round(weather?.main.temp)}°C / {Math.round(((weather?.main.temp)*9/5)+32)}°F</h2>
-      <div className='minmax'>MaxTemp: {Math.round(weather?.main.temp_max)}°C / {Math.round((weather?.main.temp_max)*9/5)+32}°F</div>
-      <div className='minmax'> MinTemp: {Math.round(weather?.main.temp_min)}°C / {Math.round((weather?.main.temp_min)*9/5)+32}°F</div>
-      <h3 className='description' style={{marginTop: "20px"}}>{weather?.weather[0].description}</h3>
+      <div style={{ fontWeight: 1000, fontSize: '30px', marginBottom: '15px', justifyContent: 'center', display: 'flex' }}>
+            {weather?.sys.country} {weather?.name}
+          </div>
+      <div className='content'>
+        <div className='left-section'>
+          <h2 className='temp'>{Math.round(weather?.main.temp)}°C / {Math.round(((weather?.main.temp) * 9 / 5) + 32)}°F</h2>
+          <div className='minmax'>최고온도: {Math.round(weather?.main.temp_max)}°C / {Math.round((weather?.main.temp_max) * 9 / 5) + 32}°F</div>
+          <div className='minmax'> 최저온도: {Math.round(weather?.main.temp_min)}°C / {Math.round((weather?.main.temp_min) * 9 / 5) + 32}°F</div>
+          <h3 className='description' style={{ marginTop: "20px" }}>{weather?.weather[0].description}</h3>
+        </div>
+        <div className='right-section'>
+          <img className='icon-image' src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt='weather icon' />
+        </div>
+      </div>
       <table>
-          <tbody>
-            <tr>
-              <th scope="col">해수면</th>
-              <th scope="col">지상층</th>
-              <th scope="col">구름양</th>
-              <th scope="col">풍속</th>
-            </tr>
-            <tr>
-              <td>{weather?.main.sea_level}hPa</td>
-              <td>{weather?.main.grnd_level}hPa</td>
-              <td>{weather?.clouds.all}%</td>
-              <td>{weather?.wind.speed}m/s</td>
-            </tr>
-          </tbody>
-        </table>
+        <tbody>
+          <tr>
+            <th scope="col">해수면</th>
+            <th scope="col">구름양</th>
+            <th scope="col">풍속</th>
+          </tr>
+          <tr>
+            <td>{weather?.main.sea_level}hPa</td>
+            <td>{weather?.clouds.all}%</td>
+            <td>{weather?.wind.speed}m/s</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
-    </div>
-  )
+  );
 }
 
-export default WeatherBox
+export default WeatherBox;
